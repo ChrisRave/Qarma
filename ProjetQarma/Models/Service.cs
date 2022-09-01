b0036b785ccce7dbb7a1fcf901e45f44e6c9a43a
GhTasnim <tasnim.gharbii@gmail.com>
2022-09-01 16:15:31 +0200

    MAJ ProjetQarmaJeudi3

---
+++ ProjetQarma/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetQarma.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public TypeService TypeService { get; set; }
+        public Bisous Bisous { get; set; }
+    }
+    public enum TypeService
+    {
+        Equipement, Hebergement, Parking, Evenement, Prestation
+    }
+}
