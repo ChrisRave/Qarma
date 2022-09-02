671bdd9621abba3d6d830a844f071576906f180d
GhTasnim <tasnim.gharbii@gmail.com>
2022-09-02 10:24:48 +0200

    Modification Vendredi 02.09

---
+++ ProjetQarma/ViewModels/ServiceViewModels.cs
@@ -0,0 +1,9 @@
+﻿using ProjetQarma.Models;
+namespace ProjetQarma.ViewModels
+{
+    public class ServiceViewModels
+    {
+        public Service Service { get; set; }
+        public Utilisateur Utilisateur { get; set; }
+    }
+}
