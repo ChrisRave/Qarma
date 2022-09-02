1812e72644510e66254e59c7412f8bd44b60ddac
ChrisRave <ravelonary_lalaina@live.fr>
2022-09-02 15:44:44 +0200

    landing avec liste services

--- ProjetQarma/ViewModels/LandingViewModel.cs
+++ ProjetQarma/ViewModels/LandingViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using ProjetQarma.Models;
 
 namespace ProjetQarma.ViewModels
 {
     public class LandingViewModel
     {
-       
-           
+        public Utilisateur Utilisateur { get; set; }
+        public List<Service> ListeSejours { get; set; }
+
     }
     
- 
+
 }
