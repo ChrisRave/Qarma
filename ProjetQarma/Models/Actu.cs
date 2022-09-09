fae625693150d9f457f7fcde8e12f6ee95a6853e
ChrisRave <ravelonary_lalaina@live.fr>
2022-09-09 12:01:31 +0200

    Classe Actualités

---
+++ ProjetQarma/Models/Actu.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetQarma.Models
+{
+    public class Actu
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public TypeActu typeActu { get; set; }
+        public string ImagePath { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+    }
+
+    public enum TypeActu
+    {
+        actualite, 
+        information, 
+        reunion, 
+        travaux, 
+    }
+}
